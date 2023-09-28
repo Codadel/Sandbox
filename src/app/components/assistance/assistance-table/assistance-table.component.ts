17faea3cc1fdc427f3d5da83f12c5501c445be8a
ADenert <adenert.pro@gmail.com>
2023-09-28 11:09:41 +0200

    feat(assistance-table): manage subscription

--- src/app/components/assistance/assistance-table/assistance-table.component.ts
+++ src/app/components/assistance/assistance-table/assistance-table.component.ts
@@ -1,7 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { BehaviorSubject, combineLatest, map, tap } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, combineLatest, map, tap } from 'rxjs';
 import { Assistance } from 'src/app/api/assistance.model';
 import { AssistanceService } from 'src/app/api/assistance.service';
 
@@ -24,40 +24,45 @@ export interface AssistanceFilters {
     )
   ]
 })
-export class AssistanceTableComponent {
+export class AssistanceTableComponent implements OnInit, OnDestroy {
 
   readonly displayedColumns = ['name', 'amount', 'expand'];
   readonly displayedColumnsWithExpand = [...this.displayedColumns, 'expand'];
   expandedElement!: Assistance | null;
+
+  readonly filters$: BehaviorSubject<AssistanceFilters> = new BehaviorSubject<AssistanceFilters>({ sort: { active: 'name', direction: 'asc' } });
+
+  assistancesSubscription!: Subscription;
+  
   dataSource!: MatTableDataSource<Assistance>;
-
-  readonly filters$ = new BehaviorSubject<AssistanceFilters>({ sort: { active: 'name', direction: 'asc' } });
-  readonly assistances$ = combineLatest({
-    filters: this.filters$.asObservable(),
-    assistances: this.assistanceService.findAll$()
-  }).pipe(
-    map(({ filters, assistances }) => {
-      let filteredAssistances = [...assistances];
-      switch (filters.sort.active) {
-        case 'name':
-          filteredAssistances.sort(function(a: Assistance, b: Assistance) {
-            if (a.organization.name.toLocaleUpperCase() > b.organization.name.toLocaleUpperCase()) {
-              return filters.sort.direction !== 'desc' ? 1 : -1;
-            } else if (a.organization.name.toLocaleUpperCase() < b.organization.name.toLocaleUpperCase()) {
-              return  filters.sort.direction !== 'desc' ? -1 : 1;
-            }
-             return 0;
-          });
-      }
-      return filteredAssistances;
-    }),
-    tap(assistances => this.dataSource = new MatTableDataSource(assistances))
-  );
-
   
   constructor(
     private assistanceService: AssistanceService
   ) {}
+
+  ngOnInit(): void {
+    this.assistancesSubscription = combineLatest({
+      filters: this.filters$.asObservable(),
+      assistances: this.assistanceService.findAll$()
+    }).pipe(
+      map(({ filters, assistances }) => {
+        let filteredAssistances = [...assistances];
+        switch (filters.sort.active) {
+          case 'name':
+            filteredAssistances.sort(function(a: Assistance, b: Assistance) {
+              if (a.organization.name.toLocaleUpperCase() > b.organization.name.toLocaleUpperCase()) {
+                return filters.sort.direction !== 'desc' ? 1 : -1;
+              } else if (a.organization.name.toLocaleUpperCase() < b.organization.name.toLocaleUpperCase()) {
+                return  filters.sort.direction !== 'desc' ? -1 : 1;
+              }
+               return 0;
+            });
+        }
+        return filteredAssistances;
+      }),
+      tap(assistances => this.dataSource = new MatTableDataSource(assistances))
+    ).subscribe();
+  }
 
   assistanceSortChanges(event: any): void {
     this.filters$.next({ ...this.filters$.value, sort: {...event} });
@@ -66,6 +71,10 @@ export class AssistanceTableComponent {
   searchFieldChanges(event: any): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnDestroy(): void {
+    this.assistancesSubscription.unsubscribe();
   }
 
 }
